35cb98c0d24d215ef9d023dc863956601250617b
Rabby-Cyber <ariyanrabby94@gmail.com>
2021-09-16 08:15:00 +0600

    fixed

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const userName = document.getElementById("username");
-const password = document.getElementById("password");
-
-const userEmail = userName.value;
-const userPass = password.value;
 const gotoNext = () => {
-  if (userEmail == "mdrabbihossen.dev@gmail.com" && userPass == "abcd@#12") {
-    window.location.href = "https://www.google.com/";
+  const userName = document.getElementById("username");
+  const password = document.getElementById("password");
+
+  const userEmail = userName.value;
+  const userPass = password.value;
+  if (userEmail === "mdrabbihossen.dev@gmail.com" && userPass === "abcd@#12") {
+    window.open("https://www.facebook.com/fazleybinmahbub26", "_blank");
   } else {
     console.log("nothing");
   }
